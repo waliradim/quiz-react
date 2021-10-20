c312f5594d876f48cd82b872d6af8e3550f5f798
waliradim <waliradim@gmail.com>
2021-10-20 11:22:31 +0600

    Login page

--- src/component/App.js
+++ src/component/App.js
@@ -1,12 +1,14 @@
 import "../styles/App.css";
 import Layout from "./Layout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 function App() {
   return (
     <Layout>
       <Home />
       <Signup />
+      <Login />
     </Layout>
   );
 }
---
+++ src/component/pages/Login.js
@@ -0,0 +1,29 @@
+import React from "react";
+import classes from "../../styles/Login.module.css";
+import Button from "../Button";
+import Form from "../Form";
+import Illustration from "../Illustration";
+import TextInput from "../TextInput";
+export default function Login() {
+  return (
+    <>
+      <h1>Login to your account</h1>
+      <div class="column">
+        <Illustration />
+        <Form className={classes.login}>
+          <TextInput
+            type="text"
+            placeholder="Enter email"
+            icon="alternate_email"
+          />
+          <TextInput type="password" placeholder="Enter password" icon="lock" />
+          <Button>Login</Button>
+
+          <div className="info">
+            Don't have an account? <a href="signup.html">Signup</a> instead.
+          </div>
+        </Form>
+      </div>
+    </>
+  );
+}
