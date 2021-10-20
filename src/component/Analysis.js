e53b680ec10df19c0d3b59544ab1203c3e46d2aa
waliradim <waliradim@gmail.com>
2021-10-20 22:10:50 +0600

    Result page

---
+++ src/component/Analysis.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Question from "./Question";
+
+export default function Analysis() {
+  return (
+    <>
+      <h1>Question Analysis</h1>
+      <h4>You answerd 5 out of 10 questions correctly</h4>
+      <Question />
+      <Question />
+      <Question />
+    </>
+  );
+}
--- src/component/App.js
+++ src/component/App.js
@@ -3,6 +3,7 @@ import Layout from "./Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
 import Signup from "./pages/Signup";
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Signup />
       <Login />
       <Quiz />
+      <Result />
     </Layout>
   );
 }
